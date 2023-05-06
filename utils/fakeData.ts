8570ce2c516bb1c94a982744c4550bde51f80aa7
leonel1221 <lzm_21@hotmail.com>
2023-05-05 20:31:20 -0500

    continuacion tabla

---
+++ utils/fakeData.ts
@@ -0,0 +1,48 @@
+const data = [
+    {
+      Identificador: "clh0zouj500003o7es6axmbci",
+      Fecha: '01/01/2021',
+      Entrada: 50,
+      Salida: 25,
+      
+    },
+    {
+      Identificador: "clh0zouj500603o7es6axmbci",
+      Fecha: '01/01/2021',
+      Entrada: 50,
+      Salida: 25,
+      
+    },
+    {
+      Identificador: "clh0zouj50303o7es6axmbci",
+      Fecha: '01/01/2021',
+      Entrada: 50,
+      Salida: 25,
+      
+    },
+    {
+      Identificador: "clh0zouj50000357es6axmbci",
+      Fecha: '01/01/2021',
+      Entrada: 50,
+      Salida: 25,
+      
+    },
+    {
+      Identificador: "clh0zouj500003o7ee6axmbci",
+      Fecha: '01/01/2021',
+      Entrada: 50,
+      Salida: 25,
+      
+    },
+    {
+      Identificador: "clh0zouj500003o7es6axmbdi",
+      Fecha: '01/01/2021',
+      Entrada: 50,
+      Salida: 25,
+      
+    },
+  
+  ];
+  
+  export { data };
+  
