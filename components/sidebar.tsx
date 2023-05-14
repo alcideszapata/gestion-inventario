0a5317535aa20d5d29e94dccc68bc8503f4dc3cf
DESKTOP-KFJE4B7\PC <charlstore@gmail.com>
2023-05-14 11:59:59 -0500

    Paginas de usuarios y materiales, botones tempora

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,23 +3,23 @@ import React from 'react'
 
 const Sidebar = () => {
 
-    return (
-      <aside className='debug text-white bg-gray-700 hidden flex-col w-64 gap-8 p-4 md:flex justify-between'>
-        <div className='flex flex-col gap-12'>
-          <div className='debug flex items-center justify-center bg-gray-900'>
-            <img src="" alt="nombre usuario" className='h-24 w-12'/>
-          </div>
-          <nav>
-            <ul className='flex flex-col gap-4'>
-              <li>Inventario</li>
-              <li>Materiales</li>
-              <li>Usuarios</li>
-            </ul>
-          </nav>
+  return (
+    <aside className='debug text-white bg-gray-700 hidden flex-col w-64 gap-8 p-4 md:flex justify-between'>
+      <div className='flex flex-col gap-12'>
+        <div className='debug flex items-center justify-center bg-gray-900'>
+          <img src="" alt="nombre usuario" className='h-24 w-12' />
         </div>
-        <button  className='flex bg-blue-500 items-center justify-center '>log out</button>
-      </aside>
-      )
+        <nav>
+          <ul className='flex flex-col gap-4'>
+            <li>Inventario</li>
+            <li>Materiales</li>
+            <li>Usuarios</li>
+          </ul>
+        </nav>
+      </div>
+      <button className='flex bg-blue-500 items-center justify-center '>log out</button>
+    </aside>
+  )
 }
 
 export default Sidebar
