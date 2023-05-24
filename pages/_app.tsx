ab070e443746eef18a3a2dda83406e570d7fc202
wulframp25 <wulfram.polo@udea.edu.co>
2023-05-23 21:15:33 -0500

    Configuración adicional de Login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+import type { AppProps } from 'next/app';
 
-import type { AppProps } from 'next/app'
-import 'styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+
+import '@styles/globals.css';
 
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+  
+
+  return (
+    <SessionProvider session={session}>      
+  
+          <Component {...pageProps} />
+
+    </SessionProvider>
+  );
+};
+
+export default App;
